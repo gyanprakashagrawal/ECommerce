ea33e338df0beaa2990f993754569117e3b0c5c6
Gyan Prakash Agrawal <73219018+gyanprakashagrawal@users.noreply.github.com>
2020-10-21 20:21:58 +0530

    user login

---
+++ User.java
@@ -0,0 +1,16 @@
+package OnlineShopping;
+
+public class User {
+
+	int userId,userPassword;
+	 
+	void login()
+	{
+		// id and password for access of website 
+	}
+	
+	void logout()
+	{
+		// logout function will be used for logging out from user id
+	}
+}
